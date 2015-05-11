29c4a3724bbc6f1e8b2be94323dd5684f4faf602
apuh001 <apuh001@ucr.edu>
2015-05-11 05:40:43 +0000

    Added minCover and displayMinCover funcs

---
+++ Lab5/lab5.h
@@ -0,0 +1,454 @@
+//  =============== BEGIN ASSESSMENT HEADER ================
+/// @file Lab5/lab5.h
+/// @brief Lab 5 Spring 2015
+///
+/// @author Alexander Puh [apuh001@ucr.edu]
+/// @date 5/4/2015
+/// @SID: 861162817
+///
+/// @par Enrollment Notes 
+///     Lecture Section: 001
+/// @par
+///     Lab Section: 022
+/// @par
+///     TA: Mandar
+//  ================== END ASSESSMENT HEADER ===============
+
+#ifndef BST_H
+#define BST_H
+#define TEST
+
+#include <cassert>
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <string>
+#include <sstream>
+#include <map>
+#include <list>
+#include <math.h>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+#define nil 0
+
+//#define Value int //Testing
+template <typename Value>
+
+class BST{
+    class Node{
+        public:
+            Value value;
+            bool selected;
+            Node* left;
+            Node* right;
+            Node(const Value v = Value()) : value(v), selected(false), 
+                                            left(nil), right(nil){}
+            Value& content(){return value;}
+            bool isInternal(){return left != nil && right != nil;}
+            bool isExternal(){return left != nil || right != nil;}
+            bool isLeaf(){return left == nil && right == nil;}
+            
+            //Helper
+            int size_subtree(Node* n)
+            {
+                if(n == nil)
+                    return 0;
+                if(n->left == nil && n->right == nil)
+                    return 1;
+        
+                //Counts itself then recursive call on its left and right nodes
+                return 1 + size_subtree(n->left) + size_subtree(n->right);
+            }
+    
+            //size of subtree rooted at this node
+            int size(){
+                //This node exists but has no left or right
+                if(left == nil && right == nil)
+                    return 1;
+                return 1 + size_subtree(root->right) + size_subtree(root->left);
+            }
+            
+            int height_(Node* curr){
+                //Base cases check to see if the next traversal exists or not
+                if(curr == nil)
+                    return -1;
+                if(curr->isLeaf())
+                    return 1;
+                //Left count and right count keep track 
+                int leftCount = 1 + height_(curr->left);
+                int rightCount = 1 + height_(curr->right);
+                
+                //Returns the greatest path
+                return static_cast<int>(fmax(leftCount, rightCount));
+            }
+            
+            //WORKING
+            int height(){
+                //height of sub tree rooted at this node
+                if(this == nil)
+                    return 0;
+                
+                //Returns counting the calling node and it's max of either 
+                //left or right subtree
+                return 1 + static_cast<int>
+                (fmax(height_(this->right), height_(this->left)));
+            }
+            
+    };
+    
+    //const Node* nil; //later nil will point to a sentinel value
+    int count;
+    Node* root;
+    
+    
+    ///////// PRIVATE HELPER FUNCS ////////////
+    
+    int size_subtree(Node* n)
+    {
+        if(n == nil)
+            return 0;
+        if(n->left == nil && n->right == nil)
+            return 1;
+        
+        //Counts itself then recursive call on its left and right nodes
+        return 1 + size_subtree(n->left) + size_subtree(n->right);
+    }
+    
+    //Helper for opeator[]
+    void subscript_helper(Node* n, vector<Node*> &vals){
+        if(n == nil)
+            return;
+            
+        if(n->isLeaf())
+        {
+            vals.push_back(n);
+            return;
+        }
+        
+        // In order push_back of vals.
+        subscript_helper(n->left, vals);
+        vals.push_back(n);
+        subscript_helper(n->right, vals);
+    }
+    
+    void preorder_(Node* n)const
+    {
+        if(n == nil)
+            return;
+            
+        if(n->isLeaf())
+        {
+            cout << n->value << endl;
+            return;
+        }
+        
+        cout << n->value << endl;
+        preorder_(n->left);
+        preorder_(n->right);
+    }
+    
+    void postorder_(Node* n)const
+    {
+        if(n == nil)
+            return;
+            
+        if(n->isLeaf())
+        {
+            cout << n->value << endl;
+            return;
+        }
+        
+        //Check left and right for leaves (left first, then right)
+        //THEN print the internal node
+        postorder_(n->left);
+        postorder_(n->right);
+        cout << n->value << endl;
+    }
+    
+     void inorder_(Node* n)const{
+        if(n == nil)
+            return;
+            
+        if(n->isLeaf())
+        {
+            cout << n->value << endl;
+            return;
+        }
+        
+        // In order... Leftmost FIRST, then the parent, and then the
+        //right subtree.
+        inorder_(n->left);
+        cout << n->value << endl;
+        inorder_(n->right);
+    }
+    
+    void displayMinCover_ (Node* n, int &count)const
+    {
+        if(n == nil)
+            return;
+            
+        if(n->isLeaf() && n->selected)
+        {
+            count++;
+            cout << n->value << endl;
+            return;
+        }
+        
+        //Inorder traversal to check cover npdes
+        displayMinCover_(n->left, count);
+        if(n->selected)
+        {
+            count++;
+            cout << n->value << endl;
+        }
+            
+        displayMinCover_(n->right,count);
+    }
+    
+    void minCover_(Node* n, bool selectAsCover = true)
+    {
+        //Root will never be passed in.
+        if(n->isLeaf() || n == nil)
+            return;
+        
+        //requirement regardless if selectAsCover is true or false
+        if(n->left != nil && n->left->isleaf())
+            n->selected = true;
+        if(n->right != nil && n->right->isleaf())
+            n->selected = true;
+        
+        //If the above steps were not done check to see if it is a cover node 
+        //or not
+        if(!n->selected)
+        {
+            if(selectAsCover)
+                n->selected
+        }
+        
+        //Flips select
+        select = !select;
+        minCover_(n->right, select);
+        minCover_(n->left, select);
+    }
+    
+    ///////END OF PRIVATE HELPER FUNCS////////////
+    
+    public:
+        void minCover()
+        {
+            //No possible cover
+            if(root = nil || root->isLeaf())
+                return;
+            minCover_(root->right, true);
+            minCover_(root->left, true);
+            return;
+        }
+        
+        void displayMinCover()
+        {
+            int coverCount = 0;
+            
+            if(root == nil)
+            {
+                cout << coverCount << endl;
+                return;
+            }
+        
+            displayMinCover_(root->left, coverCount);
+            displayMinCover_(root->right, coverCount);
+            
+            cout << coverCount << endl;
+            
+            return;
+        }
+        
+        int size(){
+            if(root == nil)
+                return 0;
+            
+            //size of tree
+            count = 1 + size_subtree(root->right) + size_subtree(root->left);
+            return 1 + size_subtree(root->right) + size_subtree(root->left);
+        }
+    
+        bool empty(){
+            return size() == 0;
+        }
+        
+        void print_node(const Node* n)
+        {
+            //print node's VALUE
+            //Formatting with spaces and endl will be up to print functions.
+            cout << n->value;
+            return;
+        }
+        
+        bool search(Value x)
+        {
+            //search for value, if found returns true
+            //If root is nil
+            if(root == nil)
+                return false;
+            
+            Node* curr = root;
+            while (curr != nil)
+            {
+                if(x < curr->value)
+                {
+                    if(curr->left != nil)
+                        curr = curr->left; 
+                    else return false;
+                }    
+                else if(x > curr->value)
+                {
+                    if(curr->right != nil)
+                        curr = curr->right;
+                    else return false;
+                }
+                else if(x == curr->value)
+                    return true;
+            }
+    
+            //Return nil if all else fails
+            return nil;
+        }
+        
+        void preorder()const{
+            //Traverse tree print one value per line in preorder
+            if(root == nil)
+                return;
+            cout << root->value << endl;
+            preorder_(root->left);
+            preorder_(root->right);
+        }
+        
+        void postorder()const{
+            //Traverse and print tree one val per line in postorder
+            if(root == nil)
+                return;
+            postorder_(root->left);
+            postorder_(root->right);
+            cout << root->value << endl;
+        }
+        
+        void inorder()const{
+            //traverse and print one val per line in inorder
+            if(root == nil)
+                return;
+            inorder_(root->left);
+            cout << root->value << endl;
+            inorder_(root->right);
+        }
+        
+        //WORKING
+        //return reference to value field of nth node
+        Value& operator[](int n)
+        {
+            assert(root != nil);
+            assert(n >= 0);
+            assert(n < size());
+            //if the size of the left subtree is smaller than the index number,
+            //indicates that n is either the root or in the right subtree
+            
+            vector<Node*> nodeVal;
+            subscript_helper(root->left, nodeVal);
+            nodeVal.push_back(root);
+            subscript_helper(root->right, nodeVal);
+            
+            return nodeVal[n]->value;
+        }
+        
+        //Default constructor
+        BST():count(0), root(nil){}
+        
+        //STUDY THIS! NODE INSERTION
+        void insert(Value X){
+            root = insert(X, root);
+        }
+        
+        Node* insert(Value X, Node* T){
+            //Normal binary tree insertion procedure
+            if(T==nil){
+                T = new Node(X);
+            }
+            else if(X < T->value){ 
+                T->left = insert(X, T->left);
+            }
+            else if(X > T->value){
+                T->right = insert(X, T->right);
+            }
+            else{
+                T->value = X;
+            }
+            
+            //Rebalance code will be put in here later.
+            
+            return T;
+        }
+        
+        //STUDY THIS! NODE REMOVAL.
+        void remove(Value X){root = remove(X, root);}
+        
+        Node* remove(Value X, Node* &T){
+            //Normal binary tree removal procedure
+            if(T != nil){
+                if(X > T->value){
+                    T->right = remove(X, T->right);
+                }
+                else if(X < T->value){
+                    T->left = remove(X, T->left);
+                }
+                // X == T->Value
+                else{
+                    if(T->right != nil){
+                        Node* x = T->right;
+                        while(x->left != nil)
+                            x = x->left;
+                        T->value = x->value;        //Successor's value
+                        T->right = remove(T->value, T->right);
+                    }
+                    else if(T->left != nil){
+                        Node* x = T->left;
+                        while(x->right != nil)
+                            x = x->right;
+                        T->value = x->value;        //Predecessor's value
+                        T->left = remove(T->value, T->left);
+                    }
+                    //*T is external
+                    else{
+                        delete T;
+                        T = nil;
+                    }
+                }
+            }
+            //Rebalance code will be put in here later.
+            return T;
+        }
+        
+        void okay(){
+            okay(root);
+        }
+        void okay(Node* T){
+            //add diagnostic code
+            cout << "Height of this tree " << T->height() << endl;
+            cout << "Printing root ";
+            print_node(root);
+            cout << endl;
+            
+            cout << "Printing root->right ";
+            print_node(root->right);
+            cout << endl;
+            cout << "Printing root->left ";
+            print_node(root->left);
+            cout << endl;
+            cout << "Printing root->left->right ";
+            print_node(root->left->right);
+            cout <<  endl;
+            return;
+        }
+};
+
+#endif
